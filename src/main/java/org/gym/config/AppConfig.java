eb9cbee115874086cc5f019c6effed15ebc83683
Tim Rj <timurrjxxx@gmail.com>
2024-02-01 23:09:12 +0500

    add dependency

--- src/main/java/org/gym/config/AppConfig.java
+++ src/main/java/org/gym/config/AppConfig.java
@@ -9,6 +9,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -19,7 +20,7 @@ import java.util.Properties;
 @ComponentScan(basePackages = "org.gym")
 @EnableTransactionManagement
 @EnableAspectJAutoProxy
-
+@EnableWebMvc
 @EnableJpaRepositories(basePackages = "org.gym.dao")
 @PropertySource(value = "classpath:application.properties")
 
--- src/main/java/org/gym/controller/TraineeController.java
+++ src/main/java/org/gym/controller/TraineeController.java
@@ -1,7 +1,18 @@
 package org.gym.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(value = "api")
 public class TraineeController {
+
+    @GetMapping
+    public ResponseEntity<String> get(){
+        String s = "HELlo";
+        return ResponseEntity.ok(s);
+    }
 }
